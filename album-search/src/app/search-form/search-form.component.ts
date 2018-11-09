9a2c3eaea7060914c1be97028f20bc1985f770d0
VredniyTony <ivanov.anton.0668556854@gmail.com>
2018-11-09 16:35:58 +0200

    multiple request & no CORS problem

--- album-search/src/app/search-form/search-form.component.ts
+++ album-search/src/app/search-form/search-form.component.ts
@@ -4,27 +4,28 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.scss']
+  styleUrls: ['./search-form.component.scss'],
 })
 
 export class SearchFormComponent implements OnInit {
 
-  private itunes_url = "https://itunes.apple.com/search?term=jack+johnson";
-  private itunes_json: any;
-  private deezer_url = "https://api.deezer.com/search?q=eminem";
-  private deezer_json: any;
   constructor(private http: HttpClient) { }
+
+  private itunes_url = "https://itunes.apple.com";
+  private deezer_url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com";
+
+  private itunes_json: any;
+  private deezer_json: any;
 
   ngOnInit() {
    }
 
    search(): void {
-    this.itunes_json = this.http.get(this.itunes_url).subscribe(json => console.log(json));;
-    // console.log(JSON.stringify(this.itunes_json));
+    let artist = (document.getElementById("artist_input") as HTMLInputElement).value
+    artist = artist.replace(" ", "+");
 
-    // this.deezer_json = this.http.get(this.deezer_url).subscribe(json => console.log(json));
-    // console.log(JSON.stringify(this.itunes_json));
+    this.itunes_json = this.http.get(this.itunes_url + "/search?term=" + artist + "&entity=album").subscribe(json => console.log(json));;
+    this.deezer_json = this.http.get(this.deezer_url + "/search/album?q=" + artist).subscribe(json => console.log(json));
 
-    console.log("hello");
   }
 } 
