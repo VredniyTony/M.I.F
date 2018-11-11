e2da3e73332b352dee8901f2df9a35b88090361f
VredniyTony <ivanov.anton.0668556854@gmail.com>
2018-11-11 22:02:38 +0200

    wrong displaying json

--- album-search/src/app/search-form/search-form.component.ts
+++ album-search/src/app/search-form/search-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { forkJoin } from "rxjs/observable/forkJoin";
 
 @Component({
   selector: 'app-search-form',
@@ -14,52 +16,83 @@ export class SearchFormComponent implements OnInit {
     itunes : "https://itunes.apple.com",
     deezer : "https://cors-anywhere.herokuapp.com/https://api.deezer.com"
   }
-
   private data = {
-    itunes : { },
-    deezer : { }
+    itunes : {},
+    deezer : {}
   }
 
-  // constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) {}
 
-  async getJson() {
-    try {
-      await this.http.get(this.url.itunes + "/search?term=" + this.artist + "&entity=album")
-        .subscribe(responseList => {
-          this.itunes = responseList;  
-      });
-      await this.http.get(this.url.deezer + "/search/album?q=" + this.artist)
-        .subscribe(responseList => {
-          this.deezer = responseList;
+  search(artist) {
+    let self = this;
+    this.artist = artist.value;
+     this.setJson().then(function() {
+      self.itunes();
+      self.deezer();
+     });
+   }
+
+  getJson(): Observable<any[]> {
+    return forkJoin(
+      [this.http.get(this.url.itunes + "/search?term=" + this.artist + "&entity=album"),
+      this.http.get(this.url.deezer + "/search/album?q=" + this.artist)]
+      );
+  }
+  setJson(){
+    return new Promise((res) => {
+        this.getJson().subscribe(responseList => {
+        this.data.itunes = responseList[0];
+        this.data.deezer = responseList[1];
+        res(); 
+        })
     });
-
-      // this.data.itunes = itunes;
-      // this.data.deezer = deezer;
-      // console.log(itunes)
-
-    } catch(e) {
-        console.log(e);
-    }
-
-  }
-
- search(artist) {
-   this.artist = artist.value;
-    this.getJson();
-    // search_album.set_json().then(function() {
-    //   search_album.itunes();
-    //   search_album.deezer();
-    // });
   }
 
   ngOnInit() {
   }
+
+  itunes() {
+    console.log(this.data.itunes)
+    // this.data.itunes.results.map(cur => {
+    //   this.draw(this.result_field.itunes, cur.collectionViewUrl, cur.collectionName)
+    // });
+  }
+  deezer() {
+    console.log(this.data.deezer)   
+    // this.data.deezer.data.map(cur => {
+    //   this.draw(this.result_field.deezer, cur.cover, cur.title)
+    // })
+  }
 }
 
-// export class albums {
+//   // draw(parent, img, title) {
+//   //   let data = `<div><img src='${img}'><p>${title}</p></div>`
+//   //   parent.insertAdjacentHTML(data)
+//   //   console.log(data);
+//   // }
+
+  //  async setJson() {
+  //   try {
+  //     await this.http.get(this.url.itunes + "/search?term=" + this.artist + "&entity=album")
+  //       .subscribe(responseList => {
+  //         this.data.itunes = responseList;  
+  //     });
+  //     await this.http.get(this.url.deezer + "/search/album?q=" + this.artist)
+  //       .subscribe(responseList => {
+  //         this.data.deezer = responseList;
+  //   });
+  //   console.log(this.data.itunes);
+  //   console.log(this.data.deezer);  
+  //   } catch(e) {
+  //       console.log(e);
+  //   }
+  // }
+
+
+
+
+  // export class albums {
   
-
-
 //   async getJson() {
 //     try {
 //       await this.http.get(this.url.itunes + "/search?term=" + this.artist + "&entity=album")
@@ -96,22 +129,3 @@ export class SearchFormComponent implements OnInit {
 //       });
 //     });
 //   }
-
-//   itunes() {
-//     console.log(this.data.itunes)
-//     // this.data.itunes.results.map(cur => {
-//     //   this.draw(this.result_field.itunes, cur.collectionViewUrl, cur.collectionName)
-//     // });
-//   }
-//   deezer() {
-//     console.log(this.data.deezer)   
-//     // this.data.deezer.data.map(cur => {
-//     //   this.draw(this.result_field.deezer, cur.cover, cur.title)
-//     // })
-//   }
-//   // draw(parent, img, title) {
-//   //   let data = `<div><img src='${img}'><p>${title}</p></div>`
-//   //   parent.insertAdjacentHTML(data)
-//   //   console.log(data);
-//   // }
-// }
