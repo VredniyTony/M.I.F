043d55898fca35e870f2379e4635b847e5fd0de8
Anton <ivanov.anton.0668556854@gmail.com>
2018-11-13 14:10:24 +0200

    code optimization

--- album-search/src/app/search-form/search-form.component.ts
+++ album-search/src/app/search-form/search-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { forkJoin } from "rxjs/observable/forkJoin";
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'app-search-form',
@@ -11,18 +11,18 @@ import { forkJoin } from "rxjs/observable/forkJoin";
 
 export class SearchFormComponent {
 
-  private artist : string;
+  private artist: string;
   public display;
   private url = {
-    itunes : "https://itunes.apple.com",
-    deezer : "https://cors-anywhere.herokuapp.com/https://api.deezer.com"
-  }
+    itunes : 'https://itunes.apple.com',
+    deezer : 'https://cors-anywhere.herokuapp.com/https://api.deezer.com'
+  };
   private data = {
     itunes : {},
     deezer : {}
-  }
+  };
 
-  constructor(private http : HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   search(artist) {
     this.display = false;
@@ -30,23 +30,23 @@ export class SearchFormComponent {
      this.setJson().then(() => {
       this.display = true;
       console.log(this.data.itunes);
-      console.log(this.data.deezer);  
+      console.log(this.data.deezer);
      });
    }
 
   getJson(): Observable<any[]> {
     return forkJoin(
-      [this.http.get(this.url.itunes + "/search?term=" + this.artist + "&entity=album"),
-      this.http.get(this.url.deezer + "/search/album?q=" + this.artist)]
+      [this.http.get(this.url.itunes + '/search?term=' + this.artist + '&entity=album'),
+      this.http.get(this.url.deezer + '/search/album?q=' + this.artist)]
       );
   }
-  setJson(){
+  setJson() {
     return new Promise((res) => {
         this.getJson().subscribe(responseList => {
         this.data.itunes = responseList[0];
         this.data.deezer = responseList[1];
-        res(); 
-        })
+        res();
+        });
     });
   }
 }
