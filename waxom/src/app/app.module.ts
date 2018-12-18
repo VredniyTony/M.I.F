1b4cc6202d3c82a1fddce1f22af572cf679adb54
VredniyTony <ivanov.anton.0668556854@gmail.com>
2018-12-18 17:58:03 +0200

    makeup header, particaly slider & service

--- waxom/src/app/app.module.ts
+++ waxom/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ServiceComponent } from './content/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
